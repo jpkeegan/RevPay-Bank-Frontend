40a3b49e56c2a335709ed36681ad259ea1ba191b
brian.spilman <brian.spilman@wsu.edu>
2023-03-29 12:30:44 -0700

    initial business loan commit

---
+++ src/components/business-loans-list.tsx
@@ -0,0 +1,19 @@
+import { BusinessLoan } from "../requests/business-loan-requests"
+
+type BusinessLoansListProps = {
+    businessLoans: BusinessLoan[]
+}
+
+export function BusinessLoansList(props: BusinessLoansListProps) {
+
+    return <>
+    
+        <ul>
+
+            {props.businessLoans.map(bl => <li key={bl.loanId}>Loan ID: {bl.loanId}<br/>Amount: ${bl.amount}<br/>Summary: {bl.summary}<br/>Business ID: {bl.businessId}</li>)}
+
+        </ul>
+    
+    </>
+
+}
---
+++ src/requests/business-loan-requests.ts
@@ -0,0 +1,63 @@
+
+export type BusinessLoan = {
+    loanId: number,
+    amount: number,
+    summary: string,
+    businessId: number
+};
+
+export type BusinessLoanForm = {
+    amount: number,
+    summary: string,
+    businessId: number
+}
+
+const url = "http://localhost:8080/";
+
+export async function createBusinessLoan(newBusinessLoan: BusinessLoanForm): Promise<BusinessLoan> {
+    const httpResponse = await fetch(url + "businessloans", {
+        method: "POST",
+        body: JSON.stringify(newBusinessLoan),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    });
+
+    const businessLoan: BusinessLoan = await httpResponse.json();
+    return businessLoan;
+}
+
+export async function getAllBusinessLoans(): Promise<BusinessLoan[]> {
+    const httpResponse = await fetch(url + "businessloans");
+    const businessLoans: BusinessLoan[] = await httpResponse.json();
+    return businessLoans;
+}
+
+export async function getBusinessLoanById(businessLoanId: number): Promise<BusinessLoan> {
+    const httpResponse = await fetch(url + "businessloans/" + businessLoanId);
+    const businessLoan: BusinessLoan = await httpResponse.json();
+    return businessLoan;
+}
+
+export async function updateBusinessLoan(businessLoan: BusinessLoan): Promise<BusinessLoan> {
+    const httpResponse = await fetch(url + "businessloans", {
+        method: "PUT",
+        body: JSON.stringify(businessLoan),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    });
+    const updatedBusinessLoan: BusinessLoan = await httpResponse.json();
+    return updatedBusinessLoan;
+}
+
+export async function deleteBusinessLoan(businessLoanId: number): Promise<boolean> {
+    const httpResponse = await fetch(url + "businessloans/" + businessLoanId, {
+        method: "DELETE",
+        headers: {
+            "Content-Type":"application/json"
+        }
+    });
+    const bool: boolean = await httpResponse.json();
+    return bool;
+}
