5945623f8700abdc5b5fa21cdd7e43fd7b2d2e56
Bridget Galang <galanbri@gmail.com>
2023-03-29 13:42:27 -0700

    added bank account form and requests

---
+++ src/pages/bank-account-form.tsx
@@ -0,0 +1,43 @@
+import { FormEvent, useState } from "react";
+import { addBankAccount } from "../requests/bank-account-requests";
+
+export type BankAccountInfo = {
+    routingNumber: number,
+    accountNumber: number,
+    accountId: number,
+}
+
+export function BankAccountForm() {
+
+    const [bankAccount, setBankAccount] = useState<BankAccountInfo>({ routingNumber: 0, accountNumber: 0, accountId: 0 });
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+        setBankAccount(prevState => ({
+            ...prevState,
+            [name]: Number(value)
+        }));
+    }
+
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        await addBankAccount(bankAccount);
+        setBankAccount({ routingNumber: 0, accountNumber: 0, accountId: 0 })
+        alert('Bank account successfully linked')
+    }
+
+    return <>
+
+        <form onSubmit={handleSubmit}>
+            <h1>Link a Bank Account</h1>
+            <label htmlFor="routingNumber">Routing Number</label>
+            <input type="number" id="routingNumber" name="routingNumber" value={bankAccount.routingNumber} onChange={handleInputChange} required/>
+            <label htmlFor="accountNumber">Account Number</label>
+            <input type="number" id="accountNumber" name="accountNumber" value={bankAccount.accountNumber} onChange={handleInputChange} required/>
+            <button type="submit">Add</button>
+        </form>
+
+    </>
+}
+
---
+++ src/requests/bank-account-requests.ts
@@ -0,0 +1,50 @@
+import { BankAccountInfo } from "../pages/bank-account-form";
+
+
+export type BankAccount = {
+    bankAccountId: number,
+    routingNumber: number,
+    accountNumber: number,
+    accountId: number,
+    balance: number
+}
+
+export const url = "http://127.0.0.1:8080";
+
+export async function addBankAccount(bankAccount: BankAccountInfo): Promise<BankAccount> {
+    const response = await fetch(`${url}/bankAccounts`, {
+        method: "POST",
+        body: JSON.stringify(bankAccount),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const newBankAccount: BankAccount = await response.json();
+    return newBankAccount;
+}
+
+export async function updateBankAccount(bankAccount: BankAccount): Promise <BankAccount> {
+    const response = await fetch(`${url}/bankAccounts/${bankAccount.bankAccountId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bankAccount)
+    });
+    
+    const updatedBankAccount: BankAccount = await response.json();
+    return updatedBankAccount;
+}
+
+export async function getBankAccountById(id: number): Promise<BankAccount> {
+    const response = await fetch(`${url}/bankAccounts/${id}`);
+    const retrievedBankAccount: BankAccount = await response.json();
+    return retrievedBankAccount;
+}
+
+export async function deleteBankAccount(id: number): Promise<void> {
+    await fetch(`${url}/bankAccounts/${id}`, {
+        method: 'DELETE'
+    });
+}
