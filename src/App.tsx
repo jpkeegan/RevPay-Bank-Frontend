6618a7218318f1d6c360c10296d92625b95bb9e6
Erich-D <33156265+Erich-D@users.noreply.github.com>
2023-03-29 09:29:46 -0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AddBusinessPage } from './pages/add-business';
+import { HomePage } from './pages/home-page';
+
+const queryClient = new QueryClient();
 
 function App() {
   return <>
-    <h1>Hello</h1>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<HomePage/>}/>
+          <Route path={'/business/new'} element={<AddBusinessPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </>
 }
 
