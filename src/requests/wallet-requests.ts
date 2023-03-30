342955faa012cbd36c54d01944dec142c00ea5c9
jpkeegan <82060340+jpkeegan@users.noreply.github.com>
2023-03-30 15:29:53 -0400

    Wallet requests, and tests(not working)

---
+++ src/requests/wallet-requests.ts
@@ -0,0 +1,49 @@
+
+const url = "http://127.0.0.1:8080";
+
+export type Wallet = {
+    walletId: number,
+    balance: number,
+    accountId: number
+};
+
+export const getWalletByAccountId = async (id: number) => {
+    const response = await fetch(`${url}/wallet/${id}`);
+    const wallet = await response.json();
+    return wallet;
+};
+export type AddWalletType = {
+    balance: number,
+    accountId: number
+}
+export const addWallet = async (newWallet: AddWalletType) => {
+    const response = await fetch(`${url}/wallets`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newWallet)
+    });
+    const wallet = await response.json();
+    return wallet;
+};
+
+export const updateWallet = async (updatedWallet: Wallet) => {
+    const response = await fetch(`${url}/wallet`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedWallet)
+    });
+    const wallet = await response.json();
+    return wallet;
+};
+
+export const deleteWallet = async (id: number) => {
+    const response = await fetch(`${url}/wallets/${id}`, {
+        method: 'DELETE',
+        headers: {"COntent-Type": "application/json"}
+    });
+    return response.ok;
+};
---
+++ src/specs/wallet-requests.spec.ts
@@ -0,0 +1,65 @@
+import { addWallet, deleteWallet, getWalletByAccountId, updateWallet, Wallet } from '../requests/wallet-requests';
+import nock from "nock";
+
+
+
+describe("Wallet API requests", () => {
+    const url = "http://127.0.0.1:8080";
+  
+    let testWallet: Wallet;
+  
+    beforeAll(async () => {
+      const testWalletData = {balance: 100, accountId: 1 };
+      nock(url)
+        .post("/wallets")
+        .reply(201, { walletId: 1, ...testWalletData });
+  
+      testWallet = await addWallet(testWalletData);
+    });
+  
+    afterAll(async () => {
+      nock(url).delete(`/wallets/${testWallet.walletId}`).reply(200);
+      await deleteWallet(testWallet.walletId);
+    });
+  
+    test("should get wallet by account id", async () => {
+      const accountId = 1;
+      const expectedWallet = { walletId:testWallet.walletId, balance: 100, accountId };
+      nock(url).get(`/wallet/${accountId}`).reply(200, expectedWallet);
+  
+      const wallet = await getWalletByAccountId(accountId);
+  
+      expect(wallet).toEqual(expectedWallet);
+    });
+  
+    test("should add wallet", async () => {
+      const newWallet = { balance: 100, accountId: 1 };
+      const expectedWallet = { walletId:testWallet.walletId, ...newWallet };
+      nock(url).post("/wallets", newWallet).reply(201, expectedWallet);
+  
+      const wallet = await addWallet(newWallet);
+  
+      expect(wallet).toEqual(expectedWallet);
+    });
+
+    test("should update wallet", async () => {
+        const updatedWallet = { walletId:testWallet.walletId, balance: 200, accountId: 1 };
+        const expectedWallet = { ...updatedWallet };
+        nock(url)
+          .put(`/wallet/${updatedWallet.walletId}`, updatedWallet)
+          .reply(200, expectedWallet);
+    
+        const wallet = await updateWallet(updatedWallet);
+    
+        expect(wallet).toEqual(expectedWallet);
+      });
+    
+      test("should delete wallet", async () => {
+        const walletId = testWallet.walletId;
+        nock(url).delete(`/wallets/${walletId}`).reply(204);
+    
+        const deleted = await deleteWallet(walletId);
+    
+        expect(deleted).toBeTruthy();
+      });
+});
