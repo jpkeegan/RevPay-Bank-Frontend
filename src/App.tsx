831d2364aab32e2324219924e9f623ae37122937
Bridget Galang <galanbri@gmail.com>
2023-03-30 09:43:23 -0700

    added route and updated form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AddBusinessPage } from './pages/add-business';
+import { BankAccountForm } from './pages/bank-account-form';
 import { HomePage } from './pages/home-page';
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path={'/'} element={<HomePage/>}/>
           <Route path={'/business/new'} element={<AddBusinessPage/>}/>
+          <Route path={'/bankAccount'} element={<BankAccountForm/>}/>
           {/* <Route path={'/loan'} element={<CreateBusinessLoanPage/>}/> */}
         </Routes>
       </BrowserRouter>
