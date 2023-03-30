8372b5bddc98e57e5ccfc244066a889046a3079c
edduclervils <edduclervils@gmail.com>
2023-03-30 17:56:25 -0400

    Finished Registration Page. Added some safety checks(unique username, password confirm, etc) stored businessAccount bool in local storage.

--- src/pages/personal-account-registration-page.tsx
+++ src/pages/personal-account-registration-page.tsx
@@ -1,8 +1,149 @@
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import { RegistrationState, userRegistrationReducer } from "../reducers/user-registration-reducer";
+import { createUserAccount, getAllUsernames, UserAccountReturnInfo } from "../requests/user-account-requests";
+
+export type UserForm = {
+    username: string
+    password: string
+    email: string
+    phoneNumber: number
+    name: string
+    address: string
+    businessAccount: boolean
+}
+
+
+const initialState: RegistrationState ={
+    confirmedPassword: "",
+    userInfo:{
+        username: "",
+        password: "",
+        email: "",
+        phoneNumber: 0,
+        name: "",
+        address:"",
+        businessAccount: false
+    }
+}
+
+
 
 
 export function PersonalRegistrationPage(){
+    const navigation = useNavigate();
+    const[trackerState, dispatch] = useReducer(userRegistrationReducer, initialState); 
+
+    function handleSetUsername(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_USERNAME", payload: event.target.value});
+    }
+    function handleSetPassword(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_PASSWORD" , payload: event.target.value});
+    }
+    function handleSetConfirmPassword(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_CONFIRM_PASSWORD", payload: event.target.value});
+    }
+    function handleSetEmail(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_EMAIL", payload: event.target.value});
+    }
+    function handleSetPhoneNumber(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_PHONE_NUMBER", payload: Number(event.target.value)});
+    }
+    function handleSetName(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_NAME", payload: event.target.value});
+    }
+    function handleSetAddress(event:React.ChangeEvent<HTMLInputElement>){
+        dispatch({type: "SET_ADDRESS", payload: event.target.value});
+    }
+
+    async function handleRegistrationParametersAction(){
+        console.log(trackerState);
+        let specialCharacterBool = false;
+        let plength = trackerState.confirmedPassword.length;
+        let existingUsers = await getAllUsernames();
+        if(trackerState.userInfo.username.indexOf(' ') >= 0){
+            alert("Cant have spaces in username.");
+            return;
+        }
+
+        if (trackerState.userInfo.username.length <= 3){
+            alert("Username should be more than 3 characters");
+            return;
+        }
+        if (trackerState.confirmedPassword !== trackerState.userInfo.password){
+            alert("Password does not match Confirmed");
+            return;
+        }
+        for(const user of existingUsers){
+            if (user.username === trackerState.userInfo.username){
+                alert("Username Already Exists.");
+                return;
+            }
+        }
+        if(plength < 8){
+            alert("Your password isn't long enough \nMust contain at least 8 characters.");
+            return
+        }
+        for (const char of trackerState.confirmedPassword){
+            if (!(char.match(/[a-z]/i))){
+                specialCharacterBool = true;
+                break;
+            }
+        }
+        if(!specialCharacterBool){
+            alert("Your password MUST contain: \n1 number.\n OR\n1 Special Character");
+            return;
+        }
+        handleRegistrationAction();
+    }   
+
+    async function handleRegistrationAction(){
+        const newUser: UserForm = {
+            username: trackerState.userInfo.username,
+            password: trackerState.userInfo.password,
+            email: trackerState.userInfo.email,
+            phoneNumber: trackerState.userInfo.phoneNumber,
+            name: trackerState.userInfo.name,
+            address:trackerState.userInfo.address,
+            businessAccount: trackerState.userInfo.businessAccount
+        }
+
+        const returnedUser:UserAccountReturnInfo = await createUserAccount(newUser);
+        localStorage.setItem("accountId",String(returnedUser.accountId));
+        localStorage.setItem("username",String(returnedUser.username));
+        localStorage.setItem("businessAccount",String(returnedUser.businessAccount));
+
+        alert("Registration Successful!!!");
+        navigation("/");
+    }
 
     return <>
         <h1>Registration Page</h1>
+        <fieldset>
+            <label htmlFor="username">USERNAME: </label>
+            <input type="text" placeholder="Username" onChange={handleSetUsername} /> <br />
+
+            <label htmlFor="password">PASSWORD: </label>
+            <input type="password" placeholder="Password" required onChange={handleSetPassword} /> <br />
+
+            <label htmlFor="confirmpassword">CONFIRM PASSWORD: </label>
+            <input type="password" placeholder="Confirm Password" onChange={handleSetConfirmPassword}/><br />
+            
+            <label htmlFor="fname">E-Mail Address: </label>
+            <input type="text" placeholder="E-Mail Address" onChange={handleSetEmail} /> <br />
+
+            <label htmlFor="lname">Phone Number: </label>
+            <input type="tel" placeholder="Phone Number" onChange={handleSetPhoneNumber}/> <br />
+
+            <label htmlFor="fname">Name: </label>
+            <input type="text" placeholder="name@place.com" onChange={handleSetName} /> <br />
+
+            <label htmlFor="lname">Address: </label>
+            <input type="text" placeholder="123 Anywhere St." onChange={handleSetAddress}/> <br />
+
+         </fieldset>
+
+         <button onClick={handleRegistrationParametersAction}>REGISTER</button>
+
     </>
 }
--- src/reducers/user-registration-reducer.ts
+++ src/reducers/user-registration-reducer.ts
@@ -1,3 +1,4 @@
+import { type } from "os"
 import { UserAccountCreationInput } from "../requests/user-account-requests"
 
 
@@ -5,10 +6,54 @@ export type RegistrationState ={
     userInfo:UserAccountCreationInput
     confirmedPassword: string
 }
-// username: string
-// password: string
-// email: string
-// phoneNumber: number
-// name: string
-// address: string
-// businessAccount: boolean
+
+export type SetUsernameAction = {type:"SET_USERNAME", payload: string};
+export type SetPasswordAction = {type:"SET_PASSWORD", payload:string};
+export type SetConfirmPasswordAction = {type:"SET_CONFIRM_PASSWORD", payload:string};
+export type SetEmailAction = {type: "SET_EMAIL", payload: string};
+export type SetPhoneNumberAction = {type: "SET_PHONE_NUMBER", payload: number};
+export type SetNameAction = {type: "SET_NAME", payload: string};
+export type SetAddressAction = {type: "SET_ADDRESS", payload: string};
+export type SetBusinessAccountAction = {type: "SET_BUSINESS_ACCOUNT", payload: boolean};
+
+export type UserAccountInfoAction = SetUsernameAction | SetPasswordAction | SetConfirmPasswordAction | SetEmailAction | SetPhoneNumberAction | SetNameAction | SetAddressAction | SetBusinessAccountAction
+
+export function userRegistrationReducer(state:RegistrationState, action: UserAccountInfoAction):RegistrationState{
+    const nextState: RegistrationState = JSON.parse(JSON.stringify(state));
+
+    switch(action.type){
+        case "SET_USERNAME": {
+            nextState.userInfo.username = action.payload;
+            return nextState;
+        }
+        case "SET_PASSWORD": {
+            nextState.userInfo.password = action.payload;
+            return nextState;
+        }
+        case "SET_CONFIRM_PASSWORD": {
+            nextState.confirmedPassword = action.payload;
+            return nextState;
+        }
+        case "SET_EMAIL": {
+            nextState.userInfo.email = action.payload;
+            return nextState;
+        }
+        case "SET_PHONE_NUMBER": {
+            nextState.userInfo.phoneNumber = action.payload;
+
+            return nextState;
+        }
+        case "SET_NAME": {
+            nextState.userInfo.name = action.payload;
+            return nextState;
+        }
+        case "SET_ADDRESS": {
+            nextState.userInfo.address = action.payload;
+            return nextState;
+        }
+        case "SET_BUSINESS_ACCOUNT": {
+            nextState.userInfo.businessAccount = action.payload;
+            return nextState;
+        }
+    }
+}
