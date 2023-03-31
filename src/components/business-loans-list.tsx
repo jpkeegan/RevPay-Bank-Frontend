3cb15c4be05c20c9bfd92f9df1aa83b855c17198
brian.spilman <brian.spilman@wsu.edu>
2023-03-31 14:26:55 -0700

    final friday commit

--- src/components/business-loans-list.tsx
+++ src/components/business-loans-list.tsx
@@ -10,7 +10,12 @@ export function BusinessLoansList(props: BusinessLoansListProps) {
     
         <ul>
 
-            {props.businessLoans.map(bl => <li key={bl.loanId}>Loan ID: {bl.loanId}<br/>Amount: ${bl.amount}<br/>Summary: {bl.summary}<br/>Business ID: {bl.businessId}</li>)}
+            {props.businessLoans.map(bl => <li key={bl.loanId}>
+                Loan ID: {bl.loanId}<br/>
+                Amount: ${bl.amount}<br/>
+                Summary: {bl.summary}<br/>
+                Business ID: {bl.businessId}<br/>
+                </li>)}
 
         </ul>
     
