96696bd3e08b362109a05d9a9be1722fbebcb9a1
Erich-D <33156265+Erich-D@users.noreply.github.com>
2023-04-03 12:55:09 -0700

    commit more

--- src/pages/update-business-page.tsx
+++ src/pages/update-business-page.tsx
@@ -1,7 +1,12 @@
-import { useQuery } from "react-query";
+import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
-import { BusinessForm, businessForm } from "../requests/types";
-import { getBusiness } from "../requests/user-requests";
+import { Checkbox } from "../components/checkbox";
+import { NavBar } from "../components/nav-bar";
+import { Form } from "../components/form-comp";
+import { BusinessEntity, BusinessForm, businessForm, businessFormDef, businessUpdateDef, CompsCheckedState } from "../requests/types";
+import { getByUsername, FailedLoginReturn } from "../requests/user-account-requests";
+import { getBusiness, insertBusinessAccount, updateBusinessAccount } from "../requests/user-requests";
 
 
 export function UpdateBusinessPage(){
@@ -9,7 +14,13 @@ export function UpdateBusinessPage(){
     const {id} = useParams();
     const Id = Number(id);
     const {isLoading, isError, data} = useQuery(["businesscache",Id], ()=>getBusiness(Id));
-    
+    const createMutation = useMutation(updateBusinessAccount, {
+        onSuccess: () => {
+            console.log("success")
+            router("/home")
+        } 
+    });
+    const [bool, setBool] = useState(!data?.isForProfit);
     if(isLoading){
         return <p>LOADING</p>
     }
@@ -17,8 +28,35 @@ export function UpdateBusinessPage(){
     if(isError){
         return <p>OH NO THERE WAS A PROBLEM</p>
     }
-     
+    console.log(data)
+    async function submitBusiness(form:BusinessForm){
+        const checkUser = async function(){
+            const result = await getByUsername(form.username)
+                if((result as FailedLoginReturn).status){
+                    console.log((result as FailedLoginReturn).status)
+                    return true
+                }
+        }
+        
+        if(localStorage.getItem("username")==form.username || await checkUser()){
+            const newBusiness:BusinessEntity = {address:form.address,email:form.email,name:form.name,
+            password:form.password,phone_number:form.phoneNumber,username:form.username,
+            bin:form.bin,ein:form.ein,businessAccount:true,isForProfit:!bool};
+            //createMutation.mutate(newBusiness);
+        }else{
+            alert("Sorry name already in use. Please try another")
+        }
+    }
+
+    function updateCheckStatus(){
+        setBool(!bool);
+    }
+ 
     return<>
-        <h1>update</h1>
+        <NavBar left={[{text:"Home",callback:()=>{router("/home")}}]}
+        right={[]} />
+        <Form def={businessUpdateDef} initState={JSON.stringify(data!)} handler={submitBusiness} buttonText={"Submit"}/>
+        <Checkbox key={data?.ein} isChecked={bool} label={"Non Profit"} checkHandler={updateCheckStatus} index={1}/>
+        
     </>
 }
